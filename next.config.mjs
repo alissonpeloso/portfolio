79ab84884318aa50191e230ec6ce4fc7b71c9d22
Alisson Luan de Lima Peloso <alisson.luan2000@gmail.com>
2025-09-03 16:52:54 -0300

    feat: add basePath and assetPrefix for production environment in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
     output: 'export',
     trailingSlash: true,
+    basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
     eslint: {
         ignoreDuringBuilds: true
     },
