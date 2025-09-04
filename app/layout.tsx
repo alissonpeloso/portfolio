d0a8e4df66c15466171ae2bc1a4a433d9f85bb8c
Alisson Luan de Lima Peloso <alisson.luan2000@gmail.com>
2025-09-04 16:51:57 -0300

    feat: add Google Tag Manager script to layout for enhanced tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistMono } from 'geist/font/mono';
 import { Suspense } from 'react';
 import './globals.css';
 import { ThemeProvider } from 'next-themes';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
     title: 'Alisson Luan de Lima Peloso - Full-Stack Developer & Tech Lead',
@@ -26,7 +27,30 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" className="scroll-smooth" suppressHydrationWarning>
+        <head>
+            <Script
+                id="gtm"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','GTM-PQWX3H4B');
+                    `,
+                }}
+            />
+        </head>
         <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
+        <noscript>
+            <iframe
+                src="https://www.googletagmanager.com/ns.html?id=GTM-PQWX3H4B"
+                height="0"
+                width="0"
+                style={{ display: 'none', visibility: 'hidden' }}
+            />
+        </noscript>
         <Suspense fallback={null}> <ThemeProvider attribute="class">{children}</ThemeProvider> </Suspense>
         </body>
         </html>
